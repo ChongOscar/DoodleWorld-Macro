bf5092372aaf0ba4df45946e6242e780d9b268de
ChongOscar <ohlookoscarshere@gmail.com>
2024-12-19 18:52:07 -0500

    added no more move detection

--- src/Macro.java
+++ src/Macro.java
@@ -10,33 +10,53 @@ public class Macro {
     }
 
     public void attack(int num) {
-        robot.mouseMove(565, 1010);
-        robot.mousePress(InputEvent.BUTTON1_DOWN_MASK); // Press the left mouse button
-        robot.mouseRelease(InputEvent.BUTTON1_DOWN_MASK); // Release the left mouse button
         robot.delay(500);
         if (num == 1) {
             robot.mouseMove(565, 875);
+            robot.mousePress(InputEvent.BUTTON1_DOWN_MASK); // Press the left mouse button
+            robot.mouseRelease(InputEvent.BUTTON1_DOWN_MASK); // Release the left mouse button
+            robot.mouseMove(560, 875);
             robot.mousePress(InputEvent.BUTTON1_DOWN_MASK); // Press the left mouse button
             robot.mouseRelease(InputEvent.BUTTON1_DOWN_MASK); // Release the left mouse button
         } else if (num == 2) {
             robot.mouseMove(1050, 875);
             robot.mousePress(InputEvent.BUTTON1_DOWN_MASK); // Press the left mouse button
             robot.mouseRelease(InputEvent.BUTTON1_DOWN_MASK); // Release the left mouse button
+            robot.mouseMove(1045, 875);
+            robot.mousePress(InputEvent.BUTTON1_DOWN_MASK); // Press the left mouse button
+            robot.mouseRelease(InputEvent.BUTTON1_DOWN_MASK); // Release the left mouse button
         } else if (num == 3) {
             robot.mouseMove(1500, 875);
+            robot.mousePress(InputEvent.BUTTON1_DOWN_MASK); // Press the left mouse button
+            robot.mouseRelease(InputEvent.BUTTON1_DOWN_MASK); // Release the left mouse button
+            robot.mouseMove(1505, 875);
             robot.mousePress(InputEvent.BUTTON1_DOWN_MASK); // Press the left mouse button
             robot.mouseRelease(InputEvent.BUTTON1_DOWN_MASK); // Release the left mouse button
         } else if (num == 4) {
             robot.mouseMove(2000, 875);
             robot.mousePress(InputEvent.BUTTON1_DOWN_MASK); // Press the left mouse button
             robot.mouseRelease(InputEvent.BUTTON1_DOWN_MASK); // Release the left mouse button
+            robot.mouseMove(2005, 875);
+            robot.mousePress(InputEvent.BUTTON1_DOWN_MASK); // Press the left mouse button
+            robot.mouseRelease(InputEvent.BUTTON1_DOWN_MASK); // Release the left mouse button
         }
+    }
+
+    public void fight() {
+        robot.mouseMove(565, 1010);
+        robot.mousePress(InputEvent.BUTTON1_DOWN_MASK); // Press the left mouse button
+        robot.mouseRelease(InputEvent.BUTTON1_DOWN_MASK); // Release the left mouse button
+        robot.mouseMove(570, 1010);
+        robot.mousePress(InputEvent.BUTTON1_DOWN_MASK); // Press the left mouse button
+        robot.mouseRelease(InputEvent.BUTTON1_DOWN_MASK); // Release the left mouse button
     }
 
     public void run() {
         robot.mouseMove(2000, 1010);
         robot.mousePress(InputEvent.BUTTON1_DOWN_MASK); // Press the left mouse button
         robot.mouseRelease(InputEvent.BUTTON1_DOWN_MASK); // Release the left mouse button
-        robot.mouseMove(2000, 1000);
+        robot.mouseMove(2005, 1010);
+        robot.mousePress(InputEvent.BUTTON1_DOWN_MASK); // Press the left mouse button
+        robot.mouseRelease(InputEvent.BUTTON1_DOWN_MASK); // Release the left mouse button
     }
 }
--- src/Runner.java
+++ src/Runner.java
@@ -6,42 +6,73 @@ import java.io.IOException;
 import java.util.Objects;
 
 public class Runner {
-    boolean run = true;
-    boolean isWhite = true;
-    boolean isNormal = true;
-    boolean isCaptured = true;
+    private boolean run;
+    private boolean isWhite;
+    private boolean isNormal;
+    private boolean isCaptured;
+    private boolean attack1Toggle;
+    private boolean attack2Toggle;
+    private boolean attack3Toggle;
+    private boolean attack4Toggle;
+    private Macro macro;
+    private ImageParser imageParser;
+    private ScreenCapture screenCapture;
+
+    public Runner() throws AWTException {
+        macro = new Macro();
+        imageParser = new ImageParser();
+        screenCapture = new ScreenCapture();
+        run = true;
+        isWhite = true;
+        isNormal = true;
+        isCaptured = true;
+        attack1Toggle = true;
+        attack2Toggle = false;
+        attack3Toggle = true;
+        attack4Toggle = true;
+    }
 
     public void run() throws AWTException, IOException, NativeHookException, InterruptedException {
         // Start global key listener
         GlobalScreen.registerNativeHook();
         GlobalScreen.addNativeKeyListener(new HotkeyListener(this));
-        String pokemonName = "nibblen";
+        String pokemonName = "Statikeet";
         while (true) {
             if (run) {
-                Macro macro = new Macro();
-                ImageParser imageParser = new ImageParser();
-                ScreenCapture screenCapture = new ScreenCapture();
-
-                screenCapture.captureImage(370, 140, 500, 60, "name");
+                String parsedPokemonName = getImageText(370, 140, 500, 60, "name");
                 screenCapture.captureImage(362, 245, 50, 50, "type");
 
-                String parsedPokemonName = imageParser.readImageText("name");
-                parsedPokemonName = parsedPokemonName.replaceAll("\\P{Print}","");
                 isWhite = imageParser.matchImageColor("name", new Color(200, 200, 200));
                 isNormal = imageParser.matchImageColor("type", new Color(0, 0, 0));
                 isCaptured = imageParser.matchImageColor("type", new Color(130, 150, 115));
+
                 System.out.println(imageParser.getAverageRGBNoBackground("name", new Color(50,102,132,255), 30));
                 System.out.println(imageParser.getAverageRGBNoBackground("type", new Color(50,102,132,255), 30));
                 System.out.println(parsedPokemonName + "\nclose enough: " +StringSimilarity.isCloseEnough(parsedPokemonName.toLowerCase(), pokemonName.toLowerCase()) + "\n" + "name white: " + isWhite + "\n" + "normal: " + isNormal+ "\n" + "captured: " + isCaptured);
+
                 if (!parsedPokemonName.isEmpty()) {
                     if (StringSimilarity.isCloseEnough(parsedPokemonName.toLowerCase(), pokemonName.toLowerCase())) {
                         if (isWhite && (isNormal || isCaptured)) {
-                            macro.attack(2);
+                            macro.fight();
+                            String attack1pp = getImageText(670, 810, 140, 40, "attack1");
+                            String attack2pp = getImageText(1140, 810, 140, 40, "attack2");
+                            String attack3pp = getImageText(1610, 810, 140, 40, "attack3");
+                            String attack4pp = getImageText(2080, 810, 140, 40, "attack4");
+                            if (isInt(attack1pp)) {
+                                if (attack1Toggle && isMoveAvaliable(attack1pp)) {
+                                    macro.attack(1);
+                                } else if (attack2Toggle && isMoveAvaliable(attack2pp)) {
+                                    macro.attack(2);
+                                } else if (attack3Toggle && isMoveAvaliable(attack3pp)) {
+                                    macro.attack(3);
+                                } else if (attack4Toggle && isMoveAvaliable(attack4pp)) {
+                                    macro.attack(4);
+                                }
+                            }
                         }
                     } else {
                         macro.run();
                     }
-                    imageParser.end();
                 }
             }
             Thread.sleep(500);
@@ -50,6 +81,36 @@ public class Runner {
 
     public void toggleRun() {
         run = !run;
+    }
+
+    private String getImageText(int x, int y, int width, int height, String name) {
+        screenCapture.captureImage(x, y, width, height, name);
+        String string = imageParser.readImageText(name);
+        System.out.println(string);
+        return string.replaceAll("\\P{Print}","");
+    }
+
+    private boolean isMoveAvaliable(String str) {
+        if (str == null || str.isEmpty()) {
+            return false;
+        }
+        try {
+            int firstNum = Integer.parseInt(str.substring(0, 1));
+            return firstNum != 0;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+    private boolean isInt(String str) {
+        if (str == null || str.isEmpty()) {
+            return false;
+        }
+        try {
+            Integer.parseInt(str.substring(0, 1));
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
     }
 }
 
